94a63ae4bde967de81a3c4dd44136e069b11184c
Yixiang Gao <yixiangg310573@gmail.com>
2026-01-20 17:48:34 -0600

    convert plot.py to python 3

--- Log/plot.py
+++ Log/plot.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 
 #######for ikfom
-fig, axs = plt.subplots(4,2)
+fig, axs = plt.subplots(4, 2, figsize=(12, 12))
 lab_pre = ['', 'pre-x', 'pre-y', 'pre-z']
 lab_out = ['', 'out-x', 'out-y', 'out-z']
 plot_ind = range(7,10)
@@ -22,12 +22,13 @@ axs[2,1].set_title('ba')
 axs[3,1].set_title('Gravity')
 for i in range(1,4):
     for j in range(8):
-        axs[j%4, j/4].plot(time, a_pre[:,i+j*3],'.-', label=lab_pre[i])
-        axs[j%4, j/4].plot(time, a_out[:,i+j*3],'.-', label=lab_out[i])
+        axs[j%4, j//4].plot(time, a_pre[:,i+j*3],'.-', label=lab_pre[i])
+        axs[j%4, j//4].plot(time, a_out[:,i+j*3],'.-', label=lab_out[i])
 for j in range(8):
     # axs[j].set_xlim(386,389)
-    axs[j%4, j/4].grid()
-    axs[j%4, j/4].legend()
+    axs[j%4, j//4].grid()
+    axs[j%4, j//4].legend()
+plt.tight_layout()
 plt.grid()
 #######for ikfom#######
 
