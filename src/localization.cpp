9be02041e988c4e05819994e02d03002d2fa4a8d
Yixiang Gao <yixiangg310573@gmail.com>
2026-01-21 13:36:33 -0600

    making the base and odom frame become parameters in fast_lio

--- src/localization.cpp
+++ src/localization.cpp
@@ -9,16 +9,16 @@ LocalizationNode::LocalizationNode() : Node("localization_node")
 
   // Parameters
   this->declare_parameter<std::string>("localization.global_frame_id", "map");
-  this->declare_parameter<std::string>("localization.odom_frame_id", "camera_init");
-  this->declare_parameter<std::string>("localization.base_frame_id", "base_link");
+  this->declare_parameter<std::string>("odom_frame_id", "camera_init");
+  this->declare_parameter<std::string>("base_frame_id", "base_link");
   this->declare_parameter<double>("localization.ndt_resolution", 1.0);
   this->declare_parameter<double>("localization.ndt_step_size", 0.1);
   this->declare_parameter<double>("localization.ndt_trans_epsilon", 0.01);
   this->declare_parameter<int>("localization.ndt_max_iter", 30);
 
   this->get_parameter("localization.global_frame_id", this->global_frame_id_);
-  this->get_parameter("localization.odom_frame_id", this->odom_frame_id_);
-  this->get_parameter("localization.base_frame_id", this->base_frame_id_);
+  this->get_parameter("odom_frame_id", this->odom_frame_id_);
+  this->get_parameter("base_frame_id", this->base_frame_id_);
   this->get_parameter("localization.ndt_resolution", this->ndt_resolution_);
   this->get_parameter("localization.ndt_step_size", this->ndt_step_size_);
   this->get_parameter("localization.ndt_trans_epsilon", this->ndt_trans_epsilon_);
